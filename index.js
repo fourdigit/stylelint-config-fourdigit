755a096d4fa535d1e4f1bbf7918f425664a23aec
yoshitaka_shinmura <yoshitaka.shinmura@4digit.jp>
2021-10-28 20:24:23 +0900

    refactor(stylelint): sort rules and prettier

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 'use strict';
-
-module.exports = {
-  plugins: ['stylelint-order', 'stylelint-scss'],
-  extends: ['stylelint-config-standard'],
+/**
+ * @type {import('stylelint').Config}
+ */
+const stylelintConfig = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier'
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss'
+  ],
   rules: {
     'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
-    'order/order': ['custom-properties', 'declarations'], // Sort alphabetically
-    'order/properties-alphabetical-order': true,
-    indentation: 2,
-    'string-quotes': 'single',
     'color-hex-case': 'lower',
     'color-hex-length': 'long',
     'color-named': 'never',
-    'selector-no-qualifying-type': true,
-    'selector-combinator-space-after': 'always',
-    'selector-attribute-quotes': 'always',
-    'selector-attribute-operator-space-before': 'never',
-    'selector-attribute-operator-space-after': 'never',
-    'selector-attribute-brackets-space-inside': 'never',
-    'declaration-block-trailing-semicolon': 'always',
-    'declaration-colon-space-before': 'never',
-    'declaration-colon-space-after': 'always-single-line',
-    'property-no-vendor-prefix': true,
-    'value-no-vendor-prefix': true,
-    'number-leading-zero': 'never',
-    'function-url-quotes': 'always',
-    'font-family-name-quotes': 'always-where-recommended',
-    'comment-whitespace-inside': 'always',
     'comment-empty-line-before': 'never',
-    'rule-empty-line-before': ['always', { ignore: ['first-nested', 'after-comment'] }],
-    'selector-pseudo-element-colon-notation': 'single',
-    'selector-pseudo-class-parentheses-space-inside': 'never',
-    'selector-no-vendor-prefix': true,
-    'media-feature-range-operator-space-before': 'never',
-    'media-feature-range-operator-space-after': 'never',
-    'media-feature-parentheses-space-inside': 'never',
-    'media-feature-name-no-vendor-prefix': true,
+    'comment-whitespace-inside': 'always',
+    'declaration-block-trailing-semicolon': 'always',
+    'declaration-colon-space-after': 'always-single-line',
+    'declaration-colon-space-before': 'never',
+    'font-family-name-quotes': 'always-where-recommended',
+    'function-url-quotes': 'always',
+    'indentation': 2,
+    'media-feature-colon-space-after': 'never',
     'media-feature-colon-space-before': 'never',
-    'media-feature-colon-space-after': 'never'
+    'media-feature-name-no-vendor-prefix': true,
+    'media-feature-parentheses-space-inside': 'never',
+    'media-feature-range-operator-space-after': 'never',
+    'media-feature-range-operator-space-before': 'never',
+    'number-leading-zero': 'never',
+    'order/order': ['custom-properties', 'declarations'], // Sort alphabetically
+    'order/properties-alphabetical-order': true,
+    'property-no-vendor-prefix': true,
+    'rule-empty-line-before': ['always', { ignore: ['first-nested', 'after-comment'] }],
+    'scss/at-rule-no-unknown': true,
+    'selector-attribute-brackets-space-inside': 'never',
+    'selector-attribute-operator-space-after': 'never',
+    'selector-attribute-operator-space-before': 'never',
+    'selector-attribute-quotes': 'always',
+    'selector-combinator-space-after': 'always',
+    'selector-no-qualifying-type': true,
+    'selector-no-vendor-prefix': true,
+    'selector-pseudo-class-parentheses-space-inside': 'never',
+    'selector-pseudo-element-colon-notation': 'single',
+    'string-quotes': 'single',
+    'value-no-vendor-prefix': true,
   }
 };
+module.exports = stylelintConfig;
